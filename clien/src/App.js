669db090b2439a47a981cc0b7d91c6f42eb3a768
vtrust <vtrust32@gmail.com>
2019-05-09 20:33:09 +0800

    add some plan

--- clien/src/App.js
+++ clien/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
+import {Button} from 'antd';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Button type = "primary">Button</Button>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
